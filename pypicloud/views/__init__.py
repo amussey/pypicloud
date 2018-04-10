570f80a7aec7c64d5f4f51e0e056170d2470aea5
Andrew Mussey <git@amussey.com>
2018-04-09 18:24:13 -0700

    Add a setting `authenticated_index` to force the index to redirect to a login page.

--- pypicloud/views/__init__.py
+++ pypicloud/views/__init__.py
@@ -1,7 +1,7 @@
 """ Views """
 import logging
 import traceback
-from pyramid.httpexceptions import HTTPException, HTTPServerError
+from pyramid.httpexceptions import HTTPException, HTTPFound, HTTPServerError
 from pyramid.settings import asbool
 from pyramid.view import view_config
 from pyramid_duh import addslash
@@ -18,6 +18,13 @@ LOG = logging.getLogger(__name__)
 @addslash
 def get_index(request):
     """ Render a home screen """
+    authenticated_index = \
+        request.registry.settings.get('pypi.authenticated_index', 'false')
+
+    if 'true' == authenticated_index.lower() and request.userid is None:
+        login_url = '{}/{}'.format(request.app_url().rstrip('/'), 'login')
+        return HTTPFound(location=login_url)
+
     return {
         'version': __version__,
     }
